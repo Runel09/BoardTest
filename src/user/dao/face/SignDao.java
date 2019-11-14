657422019cb31a73e41940122cfbbb75095b7755
Runel09 <dlrudtn95@gmail.com>
2019-11-14 09:24:55 +0900

    중간 수정

--- src/user/dao/face/SignDao.java
+++ src/user/dao/face/SignDao.java
@@ -1,5 +1,10 @@
 package user.dao.face;
 
-public interface SignDao {
+import user.dto.User;
 
+public interface SignDao {	
+	//서비스에서 보내준 아이디 정보를 DB에서 확인후 0,1로 반환(0=중복,1=비중복)
+	public int checkIdDB(User user);
+	
+	
 }
--- src/user/service/impl/SignServiceImpl.java
+++ src/user/service/impl/SignServiceImpl.java
@@ -1,7 +1,26 @@
 package user.service.impl;
 
+import user.dto.User;
 import user.service.face.SignService;
 
 public class SignServiceImpl implements SignService {
+
+	@Override
+	public void checkId(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User setAdress() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void checkAndInsert(User user) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
