2de9a4f31dede06f8950a76a80c52adc066bcfc7
bjan <bgan0604@naver.com>
2019-11-28 12:38:35 +0900

    11.28

---
+++ src/controller/login/LoginChkFormController.java
@@ -0,0 +1,25 @@
+package controller.login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginChkFormController
+ */
+@WebServlet("/Login/chk")
+public class LoginChkFormController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+		protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+			// TODO Auto-generated method stub
+		
+	
+		resp.sendRedirect("/WEB-INF/userinfo/idcheckform");
+	}
+}
