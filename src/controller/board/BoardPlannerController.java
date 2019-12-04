3723df1ab46941054cfb51f6b1fb7a932b50ff2c
TofuNoh <shtmdwo94@naver.com>
2019-12-04 18:37:03 +0900

    플래너 카테고리 초기작업 - TofuNoh - 19/12/04

--- src/controller/board/BoardPlannerController.java
+++ src/controller/board/BoardPlannerController.java
@@ -26,18 +26,25 @@ public class BoardPlannerController extends HttpServlet {
 		//요청파라미터에서 curPage를 구하고 Paging 객체 반환
 		req.setAttribute("checkboard", "플래너");
 		Paging paging = boardService.getPaging(req);
-//		System.out.println(paging);
-		
 		//Paging 객체를 MODEL값으로 지정
-		req.setAttribute("paging", paging);
-		
-		List<Board> list = boardService.getPlannerList(paging);
-		
+		req.setAttribute("paging", paging);	
+		List<Board> list = boardService.getPlannerList(paging);	
 		req.setAttribute("list", list);
-		
 		req.getRequestDispatcher("/WEB-INF/views/board/planner.jsp")
 		.forward(req, resp);
 		
+		
+//		//결과 JSON응답
+//		resp.getWriter().println("{\"result\": "+result+", \"cnt\": "+cnt+"}");
+		
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	
+	
 	}
 
 }
